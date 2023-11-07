85b7d9565d8d5d2a096ae94b7ad634cc4f29c6cc
tle09 <elena_larisa_t@yahoo.com>
2023-11-07 16:27:29 +0200

    verificare

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,4 @@
-
+const formElem = document.querySelector('.feedback-form');
+const inputElem = document.querySelector('.feedback-form input');
+const textareaElem = document.querySelector('.feedback-form textarea');
+const submitBtn = document.querySelector('.feedback-form button');
