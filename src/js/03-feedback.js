8514e9faba0d1a990fab8b56c68f3f002ea61539
tle09 <elena_larisa_t@yahoo.com>
2023-11-07 20:35:00 +0200

    gata

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,4 @@
 const formElem = document.querySelector('.feedback-form');
 const inputElem = document.querySelector('.feedback-form input');
 const textareaElem = document.querySelector('.feedback-form textarea');
-const submitBtn = document.querySelector('#feedback-form button');
+const submitBtn = document.querySelector('.feedback-form button');
