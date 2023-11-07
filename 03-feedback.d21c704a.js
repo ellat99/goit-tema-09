6b47dd5adfa1cdafca5eba32c59f9c387db06b5f
ellat99 <148704214+ellat99@users.noreply.github.com>
2023-11-07 18:35:18 +0000

    Deploying to gh-pages from @ ellat99/goit-tema-09@8514e9faba0d1a990fab8b56c68f3f002ea61539 🚀

--- 03-feedback.d21c704a.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".feedback-form"),document.querySelector(".feedback-form input"),document.querySelector(".feedback-form textarea"),document.querySelector("#feedback-form button");
-//# sourceMappingURL=03-feedback.d21c704a.js.map
