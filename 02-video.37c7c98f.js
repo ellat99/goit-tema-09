3210f4482b9468ead9518211f05497fe3a551410
ellat99 <148704214+ellat99@users.noreply.github.com>
2023-11-10 11:28:39 +0000

    Deploying to gh-pages from @ ellat99/goit-tema-09@bae1f00f9014db1efc0259d9e20f64cac490f892 🚀

--- 02-video.37c7c98f.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),t=newPlayer(e);localStorage.getItem("video-current");let o=0;console.log("savedTime=",savedTime),savedTime>0&&(o=savedTime),t.on("timeupdate",throttle((()=>{t.getCurrentTime().then((function(e){o=e})).catch((function(e){console.log(e+"occurred")})),localStorage.setItem("video-current",o.toString())}),1e3));
-//# sourceMappingURL=02-video.37c7c98f.js.map
